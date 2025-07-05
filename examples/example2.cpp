a593d03318f9f6c590fc281d8a4c6df62e574632
HrishabhMittal <hrishabhmittal.hm@gmail.com>
2025-07-05 19:34:26 +0530

    added proper resize support+precentage size support

--- examples/example2.cpp
+++ examples/example2.cpp
@@ -46,31 +46,23 @@ int main() {
     label->fg = {0, 255, 255};
     label->bg = {32, 32, 32};
     label_ptr = label.get();
-    //std::cout<<"Label:"<<label.get();
 
     auto div = std::make_unique<Div>();
-    //std::cout<<"div:"<<div.get();
 
     auto main_button = std::make_unique<Button>("Press Me!");
     main_button->onlick = &onMainButtonClick;
     main_button->fg = {0, 0, 255};
     main_button->bg = {255, 255, 0};
-    //std::cout<<"main_button:"<<main_button.get();
 
     auto quit_button = std::make_unique<Button>("Quit");
     quit_button->onlick = &onQuitButtonClick;
     quit_button->fg = {255, 255, 255};
     quit_button->bg = {255, 64, 64};
-    //std::cout<<"quit_button:"<<quit_button.get();
     
     div->add(std::move(main_button),0.75);
     div->add(std::move(quit_button),0.25);
-    //std::cout<<"after pushing\n"<<"div:"<<div.get()<<"main_button:"<<div->children[0].second.get();
-    //std::cout<<"quit_button:"<<div->children[1].second.get();
     app.add(std::move(label),8);
     app.add(std::move(div),4);
-    //std::cout<<"after pushing\n"<<"div:"<<app.children[1].second.get()<<"main_button:"<<app.children[1].second.get()->children[0].second.get();
-    //std::cout<<"quit_button:"<<app.children[1].second.get()->children[1].second.get()<<std::flush;
         
     do {
         ButtonHandler::update();
